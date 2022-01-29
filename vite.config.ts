cafd8c2beb284d11cd8c15c58d53e6e235c3ead2
templar820 <ttemplar820@gmail.com>
2022-01-29 20:32:09 +0300

    clear

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-const port = 80;
+const port = process.env.PORT || 3000;
 
 export default defineConfig({
   server: {

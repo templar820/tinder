df1ebf8a5a0677cb4dcd8ee90ebb6a19011235cf
templar820 <ttemplar820@gmail.com>
2022-01-29 19:52:44 +0300

    scroll

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 const port = process.env.PORT || 4000;
-// https://vitejs.dev/config/
+
 export default defineConfig({
   server: {
     port,
